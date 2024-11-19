5c2257f489f365af8dafb7ffd54b5adaefc22c81
Pushkar Harishchandra Bhangale <pushkarbhangale15@gmail.com>
2024-11-19 18:21:24 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`Client connected : ${socket.id}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("recieve_message",data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected : ", socket.id);
+    });
+});
+server.listen(3001, () => {
+    console.log("server is running on port 3001");
+});
