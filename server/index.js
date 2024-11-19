d7779e92a6b64d8c4307c712b516827a92d3fd70
Pushkar Harishchandra Bhangale <pushkarbhangale15@gmail.com>
2024-11-19 18:53:55 +0530

    added the get route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 const server = http.createServer(app);
 const io = new Server(server, {
